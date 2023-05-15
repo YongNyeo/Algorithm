0443e91c0fc015ed197042790b84d6299d068ada
YongNeyo <109174778+YongNyeo@users.noreply.github.com>
2023-05-15 12:18:58 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.27 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,36 +1,20 @@
-import java.util.*;
+
+import java.util.Stack;
 class Solution {
     public String solution(String number, int k) {
-        Stack<Integer>stack = new Stack<>();
-        
-        for(int i=0;i<number.length();i++){
-            while(true){
-                //비어있으면 그냥 넣어주고 끝
-                if (stack.isEmpty()){
-                    stack.push(Integer.parseInt(number.substring(i,i+1)));
-                    break;
-                }
-                //남은 문자를 다 추가해야되는 상황이면 넣어주고 끝
-                if (stack.size()+(number.length()-i)==number.length()-k){
-                    stack.push(Integer.parseInt(number.substring(i,i+1)));
-                    break;
-                }
-                // 그게 아니라면 끝에거랑 비교. 인덱스값이크다면 빼고 넣어줘야함
-                if (stack.peek()<Integer.parseInt(number.substring(i,i+1))){
-                    stack.pop();
-                }
-                // 작다면
-                else {
-                    if (stack.size()<number.length()-k){
-                        stack.push(Integer.parseInt(number.substring(i,i+1)));}
-                    break;
-                }
+        char[] result = new char[number.length() - k];
+        Stack<Character> stack = new Stack<>();
+
+        for (int i=0; i<number.length(); i++) {
+            char c = number.charAt(i);
+            while (!stack.isEmpty() && stack.peek() < c && k-- > 0) {
+                stack.pop();
             }
+            stack.push(c);
         }
-        StringBuilder sb = new StringBuilder();
-        while(!stack.isEmpty()){
-            sb.append(stack.pop());
+        for (int i=0; i<result.length; i++) {
+            result[i] = stack.get(i);
         }
-        return sb.reverse().toString();
+        return new String(result);
     }
 }
