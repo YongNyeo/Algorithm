18811585dde30d3c41082a8c7d9316d8b00282f5
YongNeyo <109174778+YongNyeo@users.noreply.github.com>
2023-06-12 11:26:08 +0900

    [unrated] Title: 요격 시스템, Time: 0.46 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
@@ -3,22 +3,19 @@ class Solution {
     public int solution(int[][] targets) {
         int answer = 0;
         int n = targets.length;
-        boolean []visited = new boolean[n];
         Arrays.sort(targets,(o1,o2)->o1[1]-o2[1]);        
 
+        int end =0;
         for (int i=0;i<n;i++){
-            if (!visited[i]){
-                visited[i] = true;
-                answer++;
-                System.out.println(i);
-                for(int j=i+1;j<n;j++){
-                    if (targets[i][1]>targets[j][0]){
-                        visited[j]=true;
-                    }
-                    else break;
-                }
+            if (end>targets[i][0]){
+                continue;
             }
-        }
+            else{
+                end = targets[i][1];
+                answer++;
+            
+            }                
+        }    
         return answer;
         
     }
