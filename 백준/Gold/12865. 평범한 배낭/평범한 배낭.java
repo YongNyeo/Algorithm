2084d5e125a02f0f8e998fd879d70994bf68e285
YongSang Kim <109174778+YongNyeo@users.noreply.github.com>
2023-12-03 22:45:40 +0900

    [Gold V] Title: 평범한 배낭, Time: 196 ms, Memory: 53768 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -2,43 +2,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.*;
 
 class Main {
-    static int[] arr;
-    static int n;
-    static int cnt = 0;
+
+    private static int n;
+    private static int k;
+
+
 
     public static void main(String[] args) throws IOException {
+
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] split = br.readLine().split(" ");
-        int n = Integer.parseInt(split[0]);
-        int k = Integer.parseInt(split[1]);
-        int max = 0;
-        int[][] arr = new int[n+1][2];
-        // 무게 기준으로 정렬. 후 value 보고 Dp풀어내는 방식
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        int[][] dp = new int[n + 1][k + 1];
+
+        int[] v = new int[n + 1];
+        int[] w = new int[n + 1];
+
         for (int i = 1; i <= n; i++) {
-            String[] s = br.readLine().split(" ");
-            arr[i][0] = Integer.parseInt(s[0]);
-            arr[i][1] = Integer.parseInt(s[1]);
+
+            StringTokenizer sk = new StringTokenizer(br.readLine());
+            v[i] = Integer.parseInt(sk.nextToken());
+            w[i] = Integer.parseInt(sk.nextToken());
+
         }
-
-        int[][]dp = new int[n+1][k+1];
-
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <=k; j++) {
+            for (int j = 1; j <= k; j++) {
                 dp[i][j] = dp[i-1][j];
-                if (j - arr[i][0]>=0) {
-                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - arr[i][0]]+arr[i][1]);
+
+                if (j -v[i]>=0) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i-1][j-v[i]]+w[i]);
                 }
             }
         }
-
-
         System.out.println(dp[n][k]);
 
     }
-
 }
+
