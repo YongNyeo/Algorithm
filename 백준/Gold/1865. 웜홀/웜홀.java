0ac43a83d82c2a1f767186d6bda51800aeb3fe0f
YongSang Kim <109174778+YongNyeo@users.noreply.github.com>
2023-12-06 15:22:19 +0900

    [Gold III] Title: 웜홀, Time: 388 ms, Memory: 39696 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.java
+++ 백준/Gold/1865. 웜홀/웜홀.java
@@ -1,84 +1,78 @@
-import java.io.BufferedWriter;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
-import java.util.ArrayList;
-public class Main {
-    static BufferedReader br;
-    public static void main(String[] args) throws Exception {
-        br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
- 
-        int T = Integer.parseInt(br.readLine());
- 
-        for (int i = 0; i < T; i++) {
-            bw.write(result() + "\n");
-        }
- 
-        bw.flush();
-        bw.close();
-        br.close();
-    }
- 
-    public static String result() throws Exception {
-        return ckMinusCycle() ? "YES" : "NO";
-    }
- 
-    public static boolean ckMinusCycle() throws Exception {
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken()); //지점의 수
-        int M = Integer.parseInt(st.nextToken()); // 도로의 개수
-        int W = Integer.parseInt(st.nextToken()); // 웜홀의 개수
- 
-        ArrayList<Edge> edge = new ArrayList<>();
-        
-        for (int i = 0; i < M + W; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int S = Integer.parseInt(st.nextToken());
-            int E = Integer.parseInt(st.nextToken());
-            int T = Integer.parseInt(st.nextToken());
-            
-            if (i >= M) {
-                edge.add(new Edge(S, E, -T)); 
-            } else {
-                edge.add(new Edge(S, E, T)); 
-                edge.add(new Edge(E, S, T)); 
+import java.util.*;
+
+class Main {
+
+    static int n, m, w;
+
+    static List<Node>[] graph;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int test = Integer.parseInt(st.nextToken());
+        for (int t = 0; t < test; t++) {
+
+            StringTokenizer sk = new StringTokenizer(bf.readLine());
+
+            n = Integer.parseInt(sk.nextToken());
+            m = Integer.parseInt(sk.nextToken());
+            w = Integer.parseInt(sk.nextToken());
+
+            graph = new ArrayList[n + 1];
+
+            for (int i = 0; i <= n; i++) {
+                graph[i] = new ArrayList<>();
             }
-            
+
+            for (int i = 0; i < m + w; i++) {
+                StringTokenizer s1 = new StringTokenizer(bf.readLine());
+                int start = Integer.parseInt(s1.nextToken());
+                int end = Integer.parseInt(s1.nextToken());
+                int time = Integer.parseInt(s1.nextToken());
+                if (i < m) {
+                    graph[start].add(new Node(end, time));
+                    graph[end].add(new Node(start, time));
+                } else
+                    graph[start].add(new Node(end, -1 * time));
+            }
+            System.out.println(bf() ? "YES" : "NO");
+
         }
- 
-        int[] times = new int[N + 1];
- 
-        boolean ck = false;
- 
-        for (int i = 1; i < N + 1; i++) {
-            for (int j = 0; j < edge.size(); j++) {
-                Edge cur = edge.get(j);
-                
-                int time = times[cur.from] + cur.time;
-                if (times[cur.to] > time) {
-                    times[cur.to] = time;
-                    
-                    if (i == N) {
-                        ck = true;
+    }
+
+    static boolean bf() { //음의 사이클이 없으면 false
+        boolean update = false;
+        int[] dist = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            update = false;
+            for (int j = 1; j <= n; j++) {
+                for (Node node : graph[j]) {
+                    if (dist[node.num] > dist[j] + node.dist) {
+                        dist[node.num] = dist[j] + node.dist;
+
+                        update= true; //음의 사이클 존재. (모두 0으로 시작해서 j==n 조건 필요 x)
                     }
                 }
             }
+            if (!update) {
+                return update;
+            }
         }
- 
-        return ck;
+        return update;
     }
 }
- 
-class Edge {
-    int from;
-    int to;
-    int time;
- 
-    Edge(int from, int to, int time) {
-        this.from = from;
-        this.to = to;
-        this.time = time;
+
+class Node {
+
+
+    int num;
+    int dist;
+
+    public Node(int num, int dist) {
+        this.num = num;
+        this.dist = dist;
     }
 }
